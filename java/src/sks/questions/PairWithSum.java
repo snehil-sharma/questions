f9a785bf8e6925e320ae337f7baaacbe341d8a11
snehil-sharma <snehil9.cse14@nituk.ac.in>
2019-12-03 23:25:37 +0530

    Updated PairWithSum.java

--- java/src/sks/questions/PairWithSum.java
+++ java/src/sks/questions/PairWithSum.java
@@ -1,3 +1,4 @@
+//Find all unique pairs in given list which equal to given sum
 package sks.questions;
 
 import java.util.ArrayList;
@@ -6,20 +7,16 @@ import java.util.HashMap;
 public class PairWithSum {
 	private final static class Pairs{
 		final int value1;
-		final int value1Pos;
 		final int value2;
-		final int value2Pos;
 		
-		Pairs(int v1, int v1p, int v2, int v2p){
+		Pairs(int v1, int v2){
 			this.value1 = v1;
-			this.value1Pos = v1p;
 			this.value2 = v2;
-			this.value2Pos = v2p;
 		}
 		
 		@Override
 		public String toString() {
-			String s = this.value1 + " " + this.value2;
+			String s = this.value1 + "+" + this.value2;
 			return s;
 		}
 	}
@@ -27,20 +24,25 @@ public class PairWithSum {
 	public static ArrayList<Pairs> getPairs(int[] arr, int sum){
 		ArrayList<Pairs> al = new ArrayList<>();
 		HashMap<Integer, Integer> hm = new HashMap<>();
+		int comp;	// complement = sum - value
 		
 		for(int i=0; i<arr.length; i++) {
+			comp = sum - arr[i];
 			if(hm.containsKey(arr[i])) {
-				al.add(new Pairs(sum-arr[i], hm.get(arr[i]), arr[i], i));
-				hm.remove(arr[i]);
+				if(hm.get(arr[i]) == 1) {
+					al.add(new Pairs(comp, arr[i]));
+					hm.put(arr[i],2);
+				}
 			}
 			else
-				hm.put(sum-arr[i], i);
+				if(!hm.containsKey(comp))
+					hm.put(comp, 1);
 		}
 		return al;
 	}
 
 	public static void main(String[] args) {
-		int[] arr = {1,1,2,4,4,4,4,7,1,1,7,7,7,7};
+		int[] arr = {1,1,2,4,4,4,4,7,1,1,7,7,6,7,6,7,2};
 		int sum = 8;
 		System.out.println(getPairs(arr, sum));
 	}
